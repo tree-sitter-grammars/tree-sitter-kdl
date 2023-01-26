6868004ee2245cbeb61242714e4b576e90916d9d
Amaan Qureshi <amaanq12@gmail.com>
2023-01-26 01:58:17 -0500

    misc: First Revision

--- grammar.js
+++ grammar.js
@@ -1,166 +1,199 @@
 /* eslint-disable camelcase */
 
+const PREC = {
+  IDENTIFIER_CHAR: 1,
+  WS: 1,
+  BARE_IDENTIFIER: 2,
+
+  NODE_SPACE: 8,
+  NODE_TERMINATOR: 8,
+
+  NODE: 9,
+};
+
 module.exports = grammar({
   name: 'kdl',
 
-  extras: ($) => [$.comment, /\s/],
+  conflicts: ($) => [
+	[$._node_space, $._linespace],
+	[$.nodes],
+	[$._node_space],
+  ],
 
-  word: ($) => $.identifier,
+  extras: ($) => [$.single_line_comment, $.multi_line_comment],
 
   rules: {
-    document: ($) => repeat($.node),
+	// nodes := linespace* (node nodes?)? linespace*
+	nodes: ($) => seq(
+        repeat($._linespace),
+        optional($.node),
+        repeat($._linespace)
+    ),
 
-    node: ($) =>
-      prec.right(
-        seq(
-          choice($.identifier, $.node_type),
-          optional('\\'),
-          repeat($.argument),
-          repeat($.property),
-          // Nodes without children are terminated by a newline, a semicolon, or the end of a file stream:
-          // optional(choice($.children, choice(';', $.newline))),
-          optional(choice($.children, ';')),
-        ),
-      ),
+	// node := ('/-' node-space*)? type? identifier (node-space+ node-prop-or-arg)* (node-space* node-children ws*)? node-space* node-terminator
+	node: ($) => prec(PREC.NODE, seq(
+		optional(seq('/-', repeat($._node_space))),
+		optional($.type),
+		$.identifier,
+		repeat(seq(repeat1($._node_space), $.node_prop_or_arg)),
+		optional(seq(repeat($._node_space), $._node_children, repeat($._ws))),
+		repeat($._node_space),
+		$._node_terminator
+	)),
+	// node-prop-or-arg := ('/-' node-space*)? (prop | value)
+	node_prop_or_arg: ($) => seq(
+		optional(seq('/-', repeat($._node_space))),
+		choice($.prop, $.value, $.type)
+	),
+	// node-children := ('/-' node-space*)? '{' nodes '}'
+	_node_children: ($) => seq(
+		optional(seq('/-', repeat($._node_space))),
+		'{',
+		optional(seq($.node)),
+		'}'
+	),
+	// node-space := ws* escline ws* | ws+
+	_node_space: ($) => prec.dynamic(PREC.NODE_SPACE, choice(
+		seq(repeat($._ws), $._escline, repeat($._ws)),
+		repeat1($._ws)
+	)),
+	// node-terminator := single-line-comment | newline | ';' | eof
+	_node_terminator: ($) => prec(PREC.NODE_TERMINATOR, choice($.single_line_comment, $._newline, ';', $._eof)),
 
-    property: ($) =>
-      seq(
-        alias(
-          choice(
-            $.identifier,
-            $._string_literal,
-          ),
-          $.property_name,
-        ),
-        '=',
-        $.argument,
-      ),
-
-    children: ($) =>
-      seq(
-        '{',
-        repeat($.node),
-        '}',
-      ),
-
-    node_type: ($) => seq('(', optional($._type), ')'),
-
-    argument: ($) =>
-      seq(
-        optional(seq('(', optional($._type), ')')),
+	// identifier := string | bare-identifier
+	identifier: ($) => choice($._string, $._bare_identifier),
+	// bare-identifier := ((identifier-char - digit - sign) identifier-char* | sign ((identifier-char - digit) identifier-char*)?) - keyword
+	_bare_identifier: ($) => prec.right(PREC.BARE_IDENTIFIER, seq(
         choice(
-          $.number,
-          $.float,
-          $.boolean,
-          $.null,
-          $._string_literal,
-          $._raw_string_literal,
-
-          $.identifier,
-        )),
-
-    number: ($) => {
-      const hex_literal = seq(
-        optional(choice('-', '+')),
-        alias('0x', $.hex_prefix),
-        /[\da-fA-F](_?[\da-fA-F])*(_?)/,
-      );
-
-      const octal_literal = seq(
-        optional(choice('-', '+')),
-        alias('0o', $.octal_prefix),
-        /[0-7](_?[0-7])*(_?)/,
-      );
-
-      const binary_literal = seq(
-        optional(choice('-', '+')),
-        alias('0b', $.binary_prefix),
-        /[0-1](_?[0-1])*(_?)/,
-      );
-
-      const decimal_digits = /\d(_?\d)*/;
-      const signed_integer = seq(optional(choice('-', '+')), decimal_digits);
-
-      const decimal_integer_literal = choice(
-        '0',
-        seq(optional('0'), /[1-9]/, optional(seq(optional('_'), decimal_digits))),
-      );
-
-      const decimal_literal = choice(
-        seq(optional(choice('-', '+')), decimal_integer_literal),
-        decimal_digits,
-        signed_integer,
-      );
-
-      return token(choice(
-        hex_literal,
-        octal_literal,
-        binary_literal,
-        decimal_literal,
-      ));
-    },
-
-    float: () => /[+-]?(\d+(\.\d+)?|\.\d+)([Ee][+-]?\d+)?/,
-
-    boolean: ($) => choice($.true, $.false),
-    true: () => 'true',
-    false: () => 'false',
-
-    null: () => 'null',
-
-    _string_literal: ($) => alias(/"([^"\\]|\\.)*"|'([^'\\]|\\.)*'/, $.string_literal),
-    // raw_string_literal
-    // defines a raw string literal as starting with the character U+0072 (r), followed by zero or more of the character U+0023 (#) and a U+0022 (double-quote) character. The raw string body can contain any sequence of Unicode characters and is terminated only by another U+0022 (double-quote) character, followed by the same number of U+0023 (#) characters that preceded the opening U+0022 (double-quote) character.5
-    // Escape characters in the raw string body are not processed.
-    // no lookahead regex so no using ? or ! in the regex
-    _raw_string_literal: ($) =>
-      alias(
-        seq(/r#*"/, /[^"]*/, /"/, /#*/),
-        $.raw_string_literal,
-      ),
-
-    // multiline string literals start and end with " and can span multiple lines
-    multiline_string_literal: ($) =>
-      token(
-        seq(
-          '"',
-          /[^"]*/,
-          '"',
+			seq(choice($._identifier_char, $._digit, $._sign), repeat($._identifier_char)),
+			seq($._sign, optional(seq(choice($._identifier_char, $._digit), repeat($._identifier_char)))),
         ),
-      ),
+        optional($.keyword)
+    )),
+	// identifier-char := unicode - linespace - [\/(){}<>;[]=,"]
+	_identifier_char: ($) => prec(PREC.IDENTIFIER_CHAR, choice(/./, $._linespace, /[^\/\(\)\{\}<>;\[\]=,\"]/)),
+	// keyword := boolean | 'null'
+	keyword: ($) => choice($.boolean, 'null'),
+	// prop := identifier '=' value
+	prop: ($) => seq($.identifier, '=', $.value),
+	// value := type? (string | number | keyword)
+	value: ($) => seq(optional($.type), choice($._string, $.number, $.keyword)),
+	// type := '(' identifier ')'
+	type: ($) => seq('(', $.identifier, ')'),
 
-    // identifier: () => /[A-Za-z_][A-Za-z0-9_]*/,
-    // backslash is a valid character in an identifier
-    // identifier: () => /[A-Za-z_][A-Za-z0-9_\\]*/,
-    // emojis are valid characters in an identifier
-    // so anything but a space, newline, or a special character
-    // identifier: () => /[^\s\[\]\{\}\(\)=,;\\]+/,
-    // there can be commas
-    // identifier: () => /[^\s\[\]\{\}\(\)=;\\]+/,
-    // identifier cannot be /- but can be --
-    // identifier: () => /[^\s\[\]\{\}\(\)=;\\\/-]+/,
-    // cannot have /- together, but - or / can be alone
-    // identifier: () => /[^\s\[\]\{\}\(\)=;\\]+/,
-    identifier: () => /(?:(?!\/-)(?!\/\*)(?!\/\/)[^\s\[\]\{\}\(\)=;\\\/])+/,
-    _type: ($) => alias($.identifier, $.type),
+	// String
+	// string := raw-string | escaped-string
+	_string: ($) => choice($.raw_string, $._escaped_string),
+	// escaped-string := '"' character* '"'
+	_escaped_string: ($) => seq('"', repeat(choice($._escape, /[^"]/)), '"'),
+	// character := '\' escape | [^\"]
+	_character: ($) => choice($._escape, /[^"]/),
+	// escape := ["\\/bfnrt] | 'u{' hex-digit{1, 6} '}'
+	_escape: () => choice(/["\\/bfnrt]/, seq('u{', repeat1(/[0-9a-fA-F]/), '}')),
+	// hex-digit := [0-9a-fA-F]
+	_hex_digit: () => /[0-9a-fA-F]/,
+	
+	// raw-string := 'r' raw-string-hash
+	raw_string: ($) => seq('r', $._raw_string_hash),
+	// raw-string-hash := '#' raw-string-hash '#' | raw-string-quotes
+	_raw_string_hash: ($) => choice(seq('#', $._raw_string_hash, '#'), $._raw_string_quotes),
+	// raw-string-quotes := '"' .* '"'
+	_raw_string_quotes: () => seq('"', /.*/, '"'),
+	
+	// number := decimal | hex | octal | binary
+	number: ($) => choice($._decimal, $._hex, $._octal, $._binary),
 
-    newline: () => token(choice('\r\n', '\n', '\r')),
+	// decimal := sign? integer ('.' integer)? exponent?
+	_decimal: ($) => seq(
+        optional($._sign),
+        $._integer,
+        optional(seq('.', $._integer)),
+        optional($._exponent)
+    ),
+	// exponent := ('e' | 'E') sign? integer
+	_exponent: ($) => seq(choice('e', 'E'), optional($._sign), $._integer),
+	// integer := digit (digit | '_')*
+	_integer: ($) => seq($._digit, repeat(choice($._digit, '_'))),
+	// digit := [0-9]
+	_digit: () => /[0-9]/,	
+	// sign := '+' | '-'
+	_sign: () => choice('+', '-'),
 
-    comment: () =>
-      token(
-        choice(
-          seq('//', /(\\(.|\r?\n)|[^\\\n])*/),
-          seq('/*', /[^*]*\*+([^/*][^*]*\*+)*/, '/'),
-          // On top of that, KDL supports /- "slashdash" comments, which can be used to comment out individual nodes, arguments, or children:
-          // seq('/-', /(\\(.|\r?\n)|[^\\\n])*/),
-          // /- either lasts until the end of the line or until the next }
-          // seq('/-', /(\\(.|\r?\n)|[^\\\n])*}/),
-          // this fails for
-          // node /- key="value" "arg"
-          // need to ignore space and quotes in /- too
-          // this can also start in the middle of a line so account for that
-          seq('/-', /(\\(.|\r?\n)|[^\\\n])*}/),
-        ),
-      ),
+	// hex := sign? '0x' hex-digit (hex-digit | '_')*
+	_hex: ($) => seq(optional($._sign), '0x', $._hex_digit, repeat(choice($._hex_digit, '_'))),
+	// octal := sign? '0o' [0-7] [0-7_]*
+	_octal: ($) => seq(optional($._sign), '0o', /[0-7]/, repeat(choice(/[0-7]/, '_'))),
+	// binary := sign? '0b' ('0' | '1') ('0' | '1' | '_')*
+	_binary: ($) => seq(optional($._sign), '0b', choice('0', '1'), repeat(choice('0', '1', '_'))),
+
+	// boolean := 'true' | 'false'
+	boolean: () => choice('true', 'false'),
+
+	// escline := '\\' ws* (single-line-comment | newline)
+	_escline: ($) => seq('\\', repeat($._ws), choice($.single_line_comment, $._newline)),
+
+	// linespace := newline | ws | single-line-comment
+	_linespace: ($) => choice($._newline, $._ws, $.single_line_comment),
+
+	// newline := See Table (All line-break white_space)
+	// Newline
+	// The following characters should be treated as new lines:
+	//
+	// Acronym	Name	Code Pt
+	// CR	Carriage Return	U+000D
+	// LF	Line Feed	U+000A
+	// CRLF	Carriage Return and Line Feed	U+000D + U+000A
+	// NEL	Next Line	U+0085
+	// FF	Form Feed	U+000C
+	// LS	Line Separator	U+2028
+	// PS	Paragraph Separator	U+2029
+	// Note that for the purpose of new lines, CRLF is considered a single newline.
+	_newline: () => choice('\r', '\n', '\r\n', '\u0085', '\u000C', '\u2028', '\u2029'),
+
+	// ws := bom | unicode-space | multi-line-comment
+	_ws: ($) => choice($._bom, $._unicode_space, $.multi_line_comment),
+
+	// bom := '\u{FEFF}'
+	_bom: () => '\u{FEFF}',
+
+	// unicode-space := See Table (All White_Space unicode characters which are not `newline`)
+	// 	Whitespace
+	// The following characters should be treated as non-Newline white space:
+	//
+	// Name	Code Pt
+	// Character Tabulation	U+0009
+	// Space	U+0020
+	// No-Break Space	U+00A0
+	// Ogham Space Mark	U+1680
+	// En Quad	U+2000
+	// Em Quad	U+2001
+	// En Space	U+2002
+	// Em Space	U+2003
+	// Three-Per-Em Space	U+2004
+	// Four-Per-Em Space	U+2005
+	// Six-Per-Em Space	U+2006
+	// Figure Space	U+2007
+	// Punctuation Space	U+2008
+	// Thin Space	U+2009
+	// Hair Space	U+200A
+	// Narrow No-Break Space	U+202F
+	// Medium Mathematical Space	U+205F
+	// Ideographic Space	U+3000
+	_unicode_space: () => choice(
+        '\u0009', '\u0020', '\u00A0', '\u1680', '\u2000', '\u2001',
+		'\u2002', '\u2003', '\u2004', '\u2005', '\u2006', '\u2007',
+		'\u2008', '\u2009', '\u200A', '\u202F', '\u205F', '\u3000',
+    ),
+
+	// single-line-comment := '//' ^newline+ (newline | eof)
+	single_line_comment: ($) => seq('//', repeat1(/[^\r\n\u0085\u000C\u2028\u2029]/), choice($._newline, $._eof)),
+	// multi-line-comment := '/*' commented-block
+	multi_line_comment: ($) => seq('/*', $.commented_block),
+	// commented-block := '*/' | (multi-line-comment | '*' | '/' | [^*/]+) commented-block
+	commented_block: ($) => choice(
+        '*/',
+        seq(choice($.multi_line_comment, '*', '/', /[^*/]+/), $.commented_block)
+    ),
+	_eof: () => token.immediate(prec(-1, '\u0000')),
   },
 });
